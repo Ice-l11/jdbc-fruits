00dfee8de0f4f5c4e2f047c1216ca5f7b30f466d
Ice-l11 <13794743109@163.com>
2022-09-20 15:22:43 +0800

    view

---
+++ src/com/lyy/view/Client.java
@@ -0,0 +1,30 @@
+package com.lyy.view;
+
+import com.lyy.controller.Menu;
+
+/**
+ * @ClassName Client
+ * @Description 启动视图
+ * @Author Ice
+ * @Date 2022/9/20 15:03
+ * @Version 1.0
+ **/
+public class Client {
+    public static void main(String[] args) {
+        Menu menu = new Menu();
+        boolean flag = true;
+        while (flag){
+            int num = menu.showMainMenu();
+            switch (num){
+                case 1 : menu.showFruitList();break;
+                case 2 : menu.addFruitCount();break;
+                case 3 : menu.showFruitByName();break;
+                case 4 : menu.deleteFruit();break;
+                case 5 : flag = menu.exit(); break;
+                default:
+                    System.out.println("输入有误！"); break;
+            }
+        }
+        System.out.println("Good Bye! Peace And Love");
+    }
+}
