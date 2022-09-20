c4ed1c9d92e809320f702259e4449e35ae5d6429
Ice-l11 <13794743109@163.com>
2022-09-20 16:38:27 +0800

    对公共部分进行抽取

--- src/com/lyy/fruit/dao/impl/FruitDaoImpl.java
+++ src/com/lyy/fruit/dao/impl/FruitDaoImpl.java
@@ -1,13 +1,8 @@
 package com.lyy.fruit.dao.impl;
 
 import com.lyy.fruit.dao.FruitDao;
+import com.lyy.fruit.dao.base.BaseDao;
 import com.lyy.fruit.pojo.Fruit;
-import com.lyy.util.JDBCUtil;
-
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -17,120 +12,37 @@ import java.util.List;
  * @Date 2022/9/20 11:00
  * @Version 1.0
  **/
-public class FruitDaoImpl implements FruitDao {
+public class FruitDaoImpl extends BaseDao<Fruit> implements FruitDao {
 
     private static final String TABLE_NAME = "t_fruit"; //表名称
 
     @Override
     public List<Fruit> getFruitList() {
-        Connection conn = null;
-        PreparedStatement ps = null;
-        ResultSet rs = null;
-        List<Fruit> fruitList = null;
-        try {
-            conn = JDBCUtil.getConnection();
-            ps = conn.prepareStatement("select fid,fname,price,fcount,remark from " + TABLE_NAME);
-            rs = ps.executeQuery();
-            fruitList = new ArrayList<>();
-            while (rs.next()){
-                int fid = rs.getInt(1);
-                String fname = rs.getString(2);
-                int price = rs.getInt(3);
-                int fcount = rs.getInt(4);
-                String remark = rs.getString(5);
-                Fruit fruit = new Fruit(fid,fname,price,fcount,remark);
-                fruitList.add(fruit);
-            }
-        }catch (Exception e){
-            e.printStackTrace();
-        }finally {
-            JDBCUtil.closeResource(conn,ps,rs);
-        }
-        return fruitList;
+        String sql = "select fid,fname,price,fcount,remark from " + TABLE_NAME;
+        return super.queryList(sql);
     }
 
     @Override
     public Fruit getFruitByName(String name) {
-        Connection conn = null;
-        PreparedStatement ps = null;
-        ResultSet rs = null;
-        Fruit fruit = null;
-        try {
-            conn = JDBCUtil.getConnection();
-            ps = conn.prepareStatement("select fid,fname,price,fcount,remark from " + TABLE_NAME +" where fname = ?");
-            ps.setObject(1,name);
-            rs = ps.executeQuery();
-            while (rs.next()){
-                int fid = rs.getInt(1);
-                String fname = rs.getString(2);
-                int price = rs.getInt(3);
-                int fcount = rs.getInt(4);
-                String remark = rs.getString(5);
-                fruit = new Fruit(fid,fname,price,fcount,remark);
-            }
-        }catch (Exception e){
-            e.printStackTrace();
-        }finally {
-            JDBCUtil.closeResource(conn,ps,rs);
-        }
-        return fruit;
+        String sql = "select fid,fname,price,fcount,remark from " + TABLE_NAME + " where fname = ?";
+        return super.queryOne(sql,name);
     }
 
     @Override
     public boolean addFruit(Fruit fruit) {
-        Connection conn = null;
-        PreparedStatement ps = null;
-        boolean flag = false;
-        try {
-            conn = JDBCUtil.getConnection();
-            ps = conn.prepareStatement("insert into " + TABLE_NAME + "(fname,price,fcount,remark) values(?,?,?,?)");
-            ps.setObject(1,fruit.getFname());
-            ps.setObject(2,fruit.getPrice());
-            ps.setObject(3,fruit.getFcount());
-            ps.setObject(4,fruit.getRemark());
-            flag = ps.executeUpdate() > 0;
-        }catch (Exception e){
-            e.printStackTrace();
-        }finally {
-            JDBCUtil.closeResource(conn,ps);
-        }
-        return flag;
+        String sql = "insert into " + TABLE_NAME + "(fname,price,fcount,remark) values(?,?,?,?)";
+        return super.executeUpdate(sql,fruit.getFname(),fruit.getPrice(),fruit.getFcount(),fruit.getRemark()) > 0;
     }
 
     @Override
     public boolean updateFruit(Fruit fruit) {
-        Connection conn = null;
-        PreparedStatement ps = null;
-        boolean flag = false;
-        try {
-            conn = JDBCUtil.getConnection();
-            ps = conn.prepareStatement("update" + TABLE_NAME + " set fcount = ? where fid = ?");
-            ps.setObject(1,fruit.getFcount());
-            ps.setObject(2,fruit.getFid());
-            flag = ps.executeUpdate() > 0;
-        }catch (Exception e){
-            e.printStackTrace();
-        }finally {
-            JDBCUtil.closeResource(conn,ps);
-        }
-        return flag;
+        String sql = "update " + TABLE_NAME + " set fcount = ? where fid = ?";
+        return super.executeUpdate(sql,fruit.getFcount(),fruit.getFid()) > 0;
     }
 
     @Override
     public boolean deleteFruit(String name) {
-        Connection conn = null;
-        PreparedStatement ps = null;
-        boolean flag = false;
-        try {
-            conn = JDBCUtil.getConnection();
-            ps = conn.prepareStatement("delete from " + TABLE_NAME + " where fname = ?");
-            ps.setObject(1,name);
-            flag = ps.executeUpdate() > 0;
-        }catch (Exception e){
-            e.printStackTrace();
-        }finally {
-            JDBCUtil.closeResource(conn,ps);
-        }
-        return flag;
+        String sql = "delete from " + TABLE_NAME + " where fname = ?";
+        return super.executeUpdate(sql,name) > 0;
     }
 }
